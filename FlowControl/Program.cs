9d891ef56dacf7748d67c3b89e00ce101d5e4d9e
Tina Bruce <tina@fishface.se>
2024-03-05 13:08:02 +0100

    Added loops to keep aasking for a valid number in CalculateGroupCosts()

--- FlowControl/Program.cs
+++ FlowControl/Program.cs
@@ -5,27 +5,27 @@
         static void Main(string[] args)
         {
             Console.WriteLine(
-                "Below you find the main menu." +
+                "Below you'll find the main menu." +
                 "\nPlease make a selection by entering a number.");
 
             while (true)
             {
                 switch (GetUserInput(
-                    "\n0 - Exit the program" +
-                    "\n1 - Get movie ticket price per age" +
-                    "\n2 - Calculate ticket costs for agroup" +
-                    "\n3 - Repeat a phrase 10 times" +
-                    "\n4 - Get the third word of a phrase" +
-                    "\n"))  
+                    "\n 0 - Exit the program" +
+                    "\n 1 - Get movie ticket price per age" +
+                    "\n 2 - Calculate ticket costs for agroup" +
+                    "\n 3 - Repeat a phrase 10 times" +
+                    "\n 4 - Get the third word of a phrase" +
+                    "\n\n"))  
                 {
                     case "0":
                         Environment.Exit(0);
                         break;
                     case "1":
-                        CheckAge();
+                        DisplayTicketPrice();
                         break;
                     case "2":
-                        CalculateCosts();
+                        CalculateGroupCosts();
                         break; 
                     case "3":
                         RepeatTenTimes();
@@ -39,104 +39,144 @@
                 }
             }
         }
+
+        private static string GetUserInput(string prompt)
+        {
+            Console.Write($"{prompt}");
+            return Console.ReadLine();
+        }
+
+        private struct Number
+        {
+            public uint givenNumber;
+            public bool isANumber;
+        }
+
+        private static Number GetNumber(string prompt)
+        {
+            // Using a struct to store and return both the number entered by user and whether it is a valid number
+            Number result;
+
+            string input = GetUserInput(prompt);
+
+            // 0 is automatically stored in givenNumber if it is something other than a uint
+            result.isANumber = uint.TryParse(input, out result.givenNumber);
+
+            return result;
+        }
+
+        private static uint GetPriceForAge(uint age)
+        {
+            uint price;
+
+            switch (age)
+            {
+                case < 5:
+                case > 100:
+                    price = 0;
+                    break;
+                case < 20:
+                    price = 80;
+                    break;
+                case > 64:
+                    price = 90;
+                    break;
+                default:
+                    price = 120;
+                    break;
+            }
+            return price;
+        }
+
+        private static void DisplayTicketPrice()
+        {
+            Number input = GetNumber("Please enter age: ");
+
+            if (input.isANumber)
+            {
+                uint age = input.givenNumber;
+
+                if (age < 5 || age > 100)
+                {
+                    Console.WriteLine("\nFree entry for this age");
+                }
+                else if (age < 20)
+                {
+                    Console.WriteLine($"\nJunior ticket fee {GetPriceForAge(age)}kr");
+                }
+                else if (age > 64)
+                {
+                    Console.WriteLine($"\nPensioner ticket fee {GetPriceForAge(age)}kr");
+                }
+                else
+                {
+                    Console.WriteLine($"\nStandard ticket fee {GetPriceForAge(age)}kr");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nThat was not a valid age.");
+            }
+        }
+
+        private static void CalculateGroupCosts()
+        {
+            uint totalCost = 0;
+            uint numberOfpeople;
+            bool isNumber;
+
+            do
+            {
+                Number enteredNumber = GetNumber("Enter number of people: ");
+                isNumber = enteredNumber.isANumber;
+                numberOfpeople = enteredNumber.givenNumber;
+            }
+            while (!isNumber);
+
+
+            for (int i = 1; i <= numberOfpeople; i++)
+            {
+                uint age;
+                bool ageIsNumber;
+
+                do
+                {
+                    Number enteredAge = GetNumber($"Enter age for person {i}: ");
+                    ageIsNumber = enteredAge.isANumber;
+                    age = enteredAge.givenNumber;
+                }
+                while (!ageIsNumber);
+
+                totalCost += GetPriceForAge(age);
+            }
+            Console.WriteLine(
+                $"\nNumber of people: {numberOfpeople}" +
+                $"\nTotal ticket costs: {totalCost}");
+        }
+
+        private static void RepeatTenTimes()
+        {
+            string input = GetUserInput("Enter phrase: ");
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.Write($"{i}.{input}, ");
+            }
+            // To get rid of a comma at the end of the last printout, print it separately outside the loop
+            Console.Write($"10.{input}\n");
+        }
+
         private static void GetThirdWord()
         {
             string input = GetUserInput("Enter phrase with a minimum of 3 words: ");
             var splitSentence = input.Trim().Split(' ');
-          
+           
             if (splitSentence.Length < 3 ) 
             {
                 Console.WriteLine("Invalid entry");
                 return;
             }
-            Console.WriteLine(splitSentence[2]);
-        }
-        private static void RepeatTenTimes()
-        {
-            string input = GetUserInput("Enter phrase: ");
-
-            for (int i = 1; i < 10; i++) 
-            {
-                Console.Write($"{i}.{input}, ");
-            }
-            Console.Write($"10.{input}");
-        }
-        private static string GetUserInput(string prompt)
-        {
-            Console.WriteLine($"{prompt}");
-            return Console.ReadLine();
-        }
-        private static uint GetNumber(string prompt)
-        {
-            uint result;
-            bool isANumber;
-
-            do
-            {
-                string input = GetUserInput(prompt);
-                isANumber = uint.TryParse(input, out result);
-            }
-            while (!isANumber);
-
-            return result;
-        }
-        private static void CalculateCosts()
-        {
-            uint numberOfpeople = GetNumber("Enter number of people: ");
-            uint totalCost = 0;
-            //List<uint> movieGoers = new List<uint>(); 
-            //movieGoers.Add(age);
-            for (int i = 1; i <= numberOfpeople; i++)
-            { 
-                uint age = GetNumber($"Enter age for person {i}: ");
-                totalCost += GetPriceForAge(age);
-            }
-            Console.WriteLine(
-                $"Number of people: {numberOfpeople}" +
-                $"\nTotal ticket costs: {totalCost}");
-        }
-        private static uint GetPriceForAge(uint age)
-        {
-            uint price;
-
-            if (age < 5 || age > 100)
-            {
-                price = 0;
-            }
-            else if (age < 20)
-            {
-                price = 80;
-            }
-            else if (age > 64)
-            {
-                price = 90;
-            }
-            else
-            {
-                price = 120;
-            }
-            return price;   
-        }
-        private static void CheckAge()
-        {
-            uint age = GetNumber("Please enter age: ");
-
-                if (age < 5 || age > 100)
-                {
-                    Console.WriteLine("Free entry for this age");
-                } 
-                else if (age < 20)
-                {
-                    Console.WriteLine("Junior ticket fee 80kr");
-                }
-                else if (age > 64)
-                {
-                    Console.WriteLine("Pensioner ticket fee 90kr");
-                }
-                else
-                {
-                    Console.WriteLine("Standard ticket fee 120kr");
-                }
+            Console.WriteLine($"Thir word: {splitSentence[2]}");
         }
     }
 }
